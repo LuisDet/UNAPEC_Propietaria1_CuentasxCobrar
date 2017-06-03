749266ae0e4bc9903cda9a8d3c382408aaa9a68c
Emmanuel Polanco Jimenez <emmanuelator@gmail.com>
2017-06-03 15:44:48 -0400

    Little Design

--- UNAPEC_Propietaria1_CuentasxCobrar/CuentasXCobrar/MainMenu.Designer.cs
+++ UNAPEC_Propietaria1_CuentasxCobrar/CuentasXCobrar/MainMenu.Designer.cs
@@ -28,7 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.transaccionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.balancesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asientosContablesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sistemaToolStripMenuItem,
+            this.clientesToolStripMenuItem,
+            this.transaccionesToolStripMenuItem,
+            this.balancesToolStripMenuItem,
+            this.asientosContablesToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sistemaToolStripMenuItem
+            // 
+            this.sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
+            this.sistemaToolStripMenuItem.Size = new System.Drawing.Size(65, 21);
+            this.sistemaToolStripMenuItem.Text = "Sistema";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(65, 21);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // transaccionesToolStripMenuItem
+            // 
+            this.transaccionesToolStripMenuItem.Name = "transaccionesToolStripMenuItem";
+            this.transaccionesToolStripMenuItem.Size = new System.Drawing.Size(101, 21);
+            this.transaccionesToolStripMenuItem.Text = "Transacciones";
+            // 
+            // balancesToolStripMenuItem
+            // 
+            this.balancesToolStripMenuItem.Name = "balancesToolStripMenuItem";
+            this.balancesToolStripMenuItem.Size = new System.Drawing.Size(70, 21);
+            this.balancesToolStripMenuItem.Text = "Balances";
+            // 
+            // asientosContablesToolStripMenuItem
+            // 
+            this.asientosContablesToolStripMenuItem.Name = "asientosContablesToolStripMenuItem";
+            this.asientosContablesToolStripMenuItem.Size = new System.Drawing.Size(131, 21);
+            this.asientosContablesToolStripMenuItem.Text = "Asientos Contables";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::CuentasXCobrar.Properties.Resources.CxC;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // MainMenu
             // 
@@ -36,14 +102,29 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sistema de Gestion de Cuentas Por Cobrar";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem transaccionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem balancesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asientosContablesToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
