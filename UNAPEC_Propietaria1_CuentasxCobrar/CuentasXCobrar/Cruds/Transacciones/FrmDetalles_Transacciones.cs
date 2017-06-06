0915dafa39356f4ed5486026c86d0240da49c074
Emmanuel Polanco Jimenez <emmanuelator@gmail.com>
2017-06-06 00:06:51 -0400

    1

---
+++ UNAPEC_Propietaria1_CuentasxCobrar/CuentasXCobrar/Cruds/Transacciones/FrmDetalles_Transacciones.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuentasXCobrar.Cruds.Transacciones
+{
+    public partial class FrmDetalles_Transacciones : Form
+    {
+        //public  movimientos { get; set; }
+        private DBCuentasxCobrarEntities entities = new DBCuentasxCobrarEntities();
+        public FrmDetalles_Transacciones()
+        {
+            InitializeComponent();
+        }
+
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void FrmDetalles_Transacciones_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
